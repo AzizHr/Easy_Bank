4556a2b7ff34d6f7bad16ddb51d22ba045dea2ef
AzizHr <azizharkati20@gmail.com>
2023-09-28 17:02:15 +0100

    EAS-32 Adding Find all missions method

--- EB_APP/src/dao/IMissionDAO.java
+++ EB_APP/src/dao/IMissionDAO.java
@@ -2,10 +2,12 @@ package dao;
 
 import entities.Mission;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IMissionDAO<T> {
 
     Optional<T> save(T t);
+    Optional<List<Mission>> findAll();
 
 }
