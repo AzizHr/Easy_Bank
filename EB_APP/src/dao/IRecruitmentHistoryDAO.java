f2cb3bafdc3466627388e105641cf36a464dc102
AzizHr <azizharkati20@gmail.com>
2023-10-05 12:23:39 +0100

    EAS-44 Adding assignAnEmployeeToAnAgency method in RecruitmentHistory Class

--- EB_APP/src/dao/IRecruitmentHistoryDAO.java
+++ EB_APP/src/dao/IRecruitmentHistoryDAO.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public interface IRecruitmentHistoryDAO<T, S> {
+public interface IRecruitmentHistoryDAO<T> {
 
-    Optional<T> assignAnEmployeeToAnAgency(S id1, S id2);
-    Optional<T>  assignAnEmployeeToAnotherAgency(S id1, S id2);
+    Optional<T> assignAnEmployeeToAnAgency(T t);
+    Optional<T>  assignAnEmployeeToAnotherAgency(T t);
     Optional<List<T>> findAll();
 
 }
--- EB_APP/src/daoImplementaion/RecruitmentHistoryDAOImp.java
+++ EB_APP/src/daoImplementaion/RecruitmentHistoryDAOImp.java
@@ -1,29 +1,47 @@
 package daoImplementaion;
 
 import dao.IRecruitmentHistoryDAO;
+import database.Database;
 import entities.RecruitmentHistory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-public class RecruitmentHistoryDAOImp implements IRecruitmentHistoryDAO<RecruitmentHistory, String> {
+public class RecruitmentHistoryDAOImp implements IRecruitmentHistoryDAO<RecruitmentHistory> {
 
+    private static final Connection connection = Database.getInstance().getConnection();
+    private static final AgencyDAOImp agencyDAOImp = new AgencyDAOImp();
+    private static final EmployeeDAOImp employeeDAOImp = new EmployeeDAOImp();
     /**
-     * @param agencyCode
-     * @param employeeCode
+     * @param recruitmentHistory
      * @return
      */
     @Override
-    public Optional<RecruitmentHistory> assignAnEmployeeToAnAgency(String agencyCode, String employeeCode) {
-        return Optional.empty();
+    public Optional<RecruitmentHistory> assignAnEmployeeToAnAgency(RecruitmentHistory recruitmentHistory) {
+        String sql = "INSERT INTO recruitment_history (agency_code, employee_code, started_at, finished_at) VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, recruitmentHistory.getAgency().getCode());
+            preparedStatement.setString(2, recruitmentHistory.getEmployee().getCode());
+            preparedStatement.setObject(3, recruitmentHistory.getRecruitedAt());
+            preparedStatement.setObject(4, recruitmentHistory.getFinishedAt());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.of(recruitmentHistory);
     }
 
     /**
-     * @param agencyCode
-     * @param employeeCode
+     * @param recruitmentHistory
      * @return
      */
     @Override
-    public Optional<RecruitmentHistory> assignAnEmployeeToAnotherAgency(String agencyCode, String employeeCode) {
+    public Optional<RecruitmentHistory> assignAnEmployeeToAnotherAgency(RecruitmentHistory recruitmentHistory) {
         return Optional.empty();
     }
 
