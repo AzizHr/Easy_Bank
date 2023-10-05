fcbeaed55e714fae43b5be465e50b53b3dd3ce81
AzizHr <azizharkati20@gmail.com>
2023-10-05 17:20:56 +0100

    EAS-49 assign an employee to another agency

--- EB_APP/src/daoImplementaion/RecruitmentHistoryDAOImp.java
+++ EB_APP/src/daoImplementaion/RecruitmentHistoryDAOImp.java
@@ -42,7 +42,19 @@ public class RecruitmentHistoryDAOImp implements IRecruitmentHistoryDAO<Recruitm
      */
     @Override
     public Optional<RecruitmentHistory> assignAnEmployeeToAnotherAgency(RecruitmentHistory recruitmentHistory) {
-        return Optional.empty();
+        String sql = "INSERT INTO recruitment_history (agency_code, employee_code, started_at, finished_at) VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, recruitmentHistory.getAgency().getCode());
+            preparedStatement.setString(2, recruitmentHistory.getEmployee().getCode());
+            preparedStatement.setObject(3, recruitmentHistory.getRecruitedAt());
+            preparedStatement.setObject(4, recruitmentHistory.getFinishedAt());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.of(recruitmentHistory);
     }
 
     /**
